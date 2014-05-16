4c8546b3dc9781f7533f89d3b28c5d297f84922c
alex dobrianski <adobri@shaw.ca>
2014-05-16 03:53:16 -0700

    reprogramm of a fly

--- STM_BTCM.c
+++ STM_BTCM.c
@@ -5626,4 +5626,139 @@ unsigned char GetBTbyte(void)
     return Data;
 }
 
+#ifdef _18F2321_18F25K20  // comaptability mode == branch on 0ffset 8 and no priority
+#pragma insertConst
 
+#pragma origin 0x7f00
+#endif
+UWORD ReadFlash(UWORD Adress)
+{
+     UWORD RdFlash;
+    //MOVLW CODE_ADDR_UPPER ; Load TBLPTR with the base
+    //MOVWF TBLPTRU ; address of the word
+    TBLPTRU = 0;
+    //MOVLW CODE_ADDR_HIGH
+    //MOVWF TBLPTRH
+    TBLPTRH = Adress>>8;
+    //MOVLW CODE_ADDR_LOW
+    //MOVWF TBLPTRL
+    TBLPTRL = Adress&0xff;
+READ_WORD:
+     #asm
+     TBLRD*+
+     #endasm// ; read into TABLAT and increment
+     RdFlash = ((UWORD)(TABLAT))<<8;
+     #asm
+     TBLRD*+ 
+     #endasm//; read into TABLAT and increment
+     RdFlash += TABLAT;
+     return RdFlash;
+}
+void EraceFlash(UWORD Adress)
+{
+     TBLPTRU = 0;// ; address of the memory block
+     TBLPTRH = Adress>>8;
+     TBLPTRL = Adress&0xff;
+ERASE_BLOCK:
+      //BSF EECON1, EEPGD ; point to Flash program memory
+     EEPGD = 1;
+     //BCF EECON1, CFGS ; access Flash program memory
+     CFGS = 0;
+     //BSF EECON1, WREN ; enable write to memory
+     WREN = 1;
+     //BSF EECON1, FREE ; enable block Erase operation
+     FREE = 1;
+     //BCF INTCON, GIE ; disable interrupts
+     GIE = 0;
+     //Required MOVLW 55h
+     EECON2 = 0x55;
+     //Sequence MOVWF EECON2 ; write 55h
+     //MOVLW 0AAh
+     //MOVWF EECON2 ; write 0AAh
+     EECON2 = 0xaa;
+     //BSF EECON1, WR ; start erase (CPU stall)
+     WR = 1;
+     //BSF INTCON, GIE ; re-enable interrupts
+     GIE = 1;
+}
+void WriteFlash(UWORD Adress, UWORD value)
+{
+/*
+MOVLW D'64’ ; number of bytes in erase block
+MOVWF COUNTER
+MOVLW BUFFER_ADDR_HIGH ; point to buffer
+MOVWF FSR0H
+MOVLW BUFFER_ADDR_LOW
+MOVWF FSR0L
+MOVLW CODE_ADDR_UPPER ; Load TBLPTR with the base
+MOVWF TBLPTRU ; address of the memory block
+MOVLW CODE_ADDR_HIGH
+MOVWF TBLPTRH
+MOVLW CODE_ADDR_LOW
+MOVWF TBLPTRL
+READ_BLOCK
+TBLRD*+ ; read into TABLAT, and inc
+MOVF TABLAT, W ; get data
+MOVWF POSTINC0 ; store data
+DECFSZ COUNTER ; done?
+BRA READ_BLOCK ; repeat
+MODIFY_WORD
+MOVLW BUFFER_ADDR_HIGH ; point to buffer
+MOVWF FSR0H
+MOVLW BUFFER_ADDR_LOW
+MOVWF FSR0L
+MOVLW NEW_DATA_LOW ; update buffer word
+MOVWF POSTINC0
+MOVLW NEW_DATA_HIGH
+MOVWF INDF0
+ERASE_BLOCK
+MOVLW CODE_ADDR_UPPER ; load TBLPTR with the base
+MOVWF TBLPTRU ; address of the memory block
+MOVLW CODE_ADDR_HIGH
+MOVWF TBLPTRH
+MOVLW CODE_ADDR_LOW
+MOVWF TBLPTRL
+BSF EECON1, EEPGD ; point to Flash program memory
+BCF EECON1, CFGS ; access Flash program memory
+BSF EECON1, WREN ; enable write to memory
+BSF EECON1, FREE ; enable Erase operation
+BCF INTCON, GIE ; disable interrupts
+MOVLW 55h
+Required MOVWF EECON2 ; write 55h
+Sequence MOVLW 0AAh
+MOVWF EECON2 ; write 0AAh
+BSF EECON1, WR ; start erase (CPU stall)
+BSF INTCON, GIE ; re-enable interrupts
+TBLRD*- ; dummy read decrement
+MOVLW BUFFER_ADDR_HIGH ; point to buffer
+MOVWF FSR0H
+MOVLW BUFFER_ADDR_LOW
+MOVWF FSR0L
+WRITE_BUFFER_BACK
+MOVLW BlockSize ; number of bytes in holding register
+MOVWF COUNTER
+MOVLW D’64’/BlockSize ; number of write blocks in 64 bytes
+MOVWF COUNTER2
+WRITE_BYTE_TO_HREGS
+MOVF POSTINC0, W ; get low byte of buffer data
+MOVWF TABLAT ; present data to table latch
+TBLWT+* ; write data, perform a short write
+; to internal TBLWT holding register.
+DECFSZ COUNTER ; loop until holding registers are full
+BRA WRITE_WORD_TO_HREGS
+PROGRAM_MEMORY
+BSF EECON1, EEPGD ; point to Flash program memory
+BCF EECON1, CFGS ; access Flash program memory
+BSF EECON1, WREN ; enable write to memory
+BCF INTCON, GIE ; disable interrupts
+MOVLW 55h
+Required MOVWF EECON2 ; write 55h
+Sequence MOVLW 0AAh
+MOVWF EECON2 ; write 0AAh
+BSF EECON1, WR ; start program (CPU stall)
+DCFSZ COUNTER2 ; repeat for remaining write blocks
+BRA WRITE_BYTE_TO_HREGS ;
+BSF INTCON, GIE ; re-enable interrupts
+BCF EECON1, WREN ; disable write to memory
+*/
+}
